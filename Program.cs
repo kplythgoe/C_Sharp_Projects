ca235f67642279aa72b77ce412d88a8a0d28b14c
Kieran Lythgoe <91622486+kplythgoe@users.noreply.github.com>
2024-05-14 17:23:27 +0100

    Add files via upload
    This program is a  simple random number guesser as an introduction to the C# language. The console prompts the user to guess the number between 1 and 10. The program will continue to loop until the user is correct.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Creating an instance of the Random Class
+Random random = new Random();
+
+// Get a random number between 1 and 10 (including 1 and 10)
+int randomNumber = random.Next(1, 11);
+
+bool win = false;
+
+while (!win)
+{
+    Console.Write("Guess the number: ");
+    string inputString = Console.ReadLine();
+    int num1 = 0;
+
+    bool isNumber = int.TryParse(inputString, out num1);
+
+    if (isNumber)
+    {
+        if (num1 == randomNumber)
+        {
+            Console.WriteLine("You guessed right");
+            win = true;
+        }
+        else
+        {
+            Console.WriteLine("You guessed wrong, try again\n");
+        }
+    }
+    else
+    {
+        Console.WriteLine("You must enter a number\n");
+    }
+}
+
+Console.ReadLine();
